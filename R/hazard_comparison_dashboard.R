b5bb3d11538c6598fd4daf5331d8060c9cad57fd
Paul Kruse <kruse.paul@epa.gov>
2022-05-04 14:04:03 -0400

    Added helper function for preparing SMILES strings for use in API calls. Also added a function for getting toxprint data using SMILES string input. Added documentation.

--- R/hazard_comparison_dashboard.R
+++ R/hazard_comparison_dashboard.R
@@ -66,3 +66,76 @@ get_chemical_identifiers <- function(input, type = c('AnyId', 'DTXSID', 'CAS')){
 
 
 }
+
+#' Helper function for converting SMILES string to API acceptable format
+#'
+#' @param smiles A string representing a chemical in SMILES format
+#' @return A string converted for use in API calls.
+#' @import stringr
+convert_smiles_to_hex <- function(smiles){
+    hex_replacement <- c('[(]' = '%28',
+                            '[)]' = '%29',
+                            '\\[' = '%5b',
+                            '[]]' = '%5d',
+                            '[\\.]' = '%2e',
+                            '[-]' = '%2d',
+                            '[=]' = '%3d',
+                            '[#]' = '%23',
+                            '[$]' = '%24',
+                            '[:]' = '%3a',
+                            '[/]' = '%2f',
+                            '[\\\\]' = '%5c',
+                            '[@]' = '%40')
+    new_smiles <- stringr::str_replace_all(smiles, hex_replacement)
+    return(new_smiles)
+}
+
+
+#' A function for retrieving toxprint data for a given chemical
+#'
+#' @param smiles A string representing a chemical in SMILES format
+#' @return A list of chemical descriptors, including ToxPrints descriptors
+#' @export
+#' @import httr
+#' @import crayon
+#' @import clisymbols
+get_toxprints <- function(smiles) {
+  entity_url <- 'https://hazard.sciencedataexperts.com/api/descriptors?smiles='
+
+  converted_smiles <- convert_smiles_to_hex(smiles)
+
+  entity_query <- paste0(entity_url, converted_smiles, '&type=toxprints&headers=false')
+  #print(entity_query)
+  response <- httr::RETRY(
+    verb = 'get',
+    url = entity_query,
+    times = 10,
+    terminate_on = c(404),
+    quiet = T
+  )
+
+  #return(response)
+
+  if (response$status_code == 429){
+    stop('Request rate limit exceeded!')
+  }
+
+  if (response$status_code == 404){
+    cat(crayon::red(clisymbols::symbol$cross, smiles), '\n')
+  }
+
+  if (response$status_code == 200){
+    content <- httr::content(response)
+
+    if (length(content) == 0){
+      cat(crayon::red(clisymbols::symbol$cross, smiles), '\n')
+      return(invisible(NULL))
+    } else {
+      cat(crayon::green(clisymbols::symbol$tick, smiles), '\n')
+    }
+
+    information <- content$chemicals[[1]]
+    return(information)
+  }
+
+}
