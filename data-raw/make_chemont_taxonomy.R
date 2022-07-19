1c5abfb80455e58f0a8d293cad8af1980f0103d2
cring <Ring.Caroline@epa.gov>
2022-07-19 14:39:59 -0400

    Add to data-raw/make_chemont_taxonomy.R script: make chemont_tree object and save it in data/

--- data-raw/make_chemont_taxonomy.R
+++ data-raw/make_chemont_taxonomy.R
@@ -3,3 +3,6 @@ library(jsonlite)
 chemont_df <- jsonlite::fromJSON("http://classyfire.wishartlab.com/tax_nodes.json")
 names(chemont_df) <- c("Name", "ID", "Parent_ID")
 usethis::use_data(chemont_df, overwrite = TRUE)
+
+chemont_tree <- generate_taxonomy_tree(tax_nodes = chemont_df)
+usethis::use_data(chemont_tree, overwrite = TRUE)
