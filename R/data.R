ad73ec50e4cb7eaa45fc130557bab806545467b1
Paul Kruse <kruse.paul@epa.gov>
2022-09-27 15:16:26 -0400

    Added Roxygen documentation for several data sets.

--- R/data.R
+++ R/data.R
@@ -1,3 +1,78 @@
+#' biosolids chemical list from the US EPA CompTox Chemicals Dashboard
+#'
+#' A data set containing a list of 726 chemicals with related information
+#'
+#' \url{https://comptox.epa.gov/dashboard/chemical-lists/BIOSOLIDS}
+#'
+#' @format A csv representing 726 rows and 18 columns
+#' \describe{
+#' \item{DTXSID}{A chemical identifier}
+#' \item{PREFERRED.NAME}{A character string of the name}
+#' \item{CASRN}{A character string of the CASRN identifier}
+#' \item{INCHIKEY}{A character of the InChIKey identifier}
+#' \item{IUPAC.NAME}{A character string of the IUPAC identifier}
+#' \item{SMILES}{A character string of the SMILES identifier}
+#' \item{INCHI.STRING}{A character giving the INCHI string identifier.}
+#' \item{MOLECULAR.FORMULA}{A character string of the molecular formula}
+#' \item{AVERAGE.MASS}{A numeric giving the average atomic mass}
+#' \item{MONOISOTOPIC.MASS}{A numeric giving the monoisotopic mass}
+#' \item{Num.of.SOURCES}{An integer giving the number of sources}
+#' \item{Num.of.PUBMED.ARTICLES}{An integer giving the number of PUBMED
+#' Articles}
+#' \item{PUBCHEM.DATA.SOURCES}{An integer giving the number of PUBCHEM data
+#' sources}
+#' \item{CPDAT.COUNT}{An integer giving the number of occurrences within CPDAT}
+#' \item{QC.Level}{An integer giving the quality control level}
+#' \item{Num.ToxCast.Active}{???}
+#' \item{Pct.ToxCast.Active}{???}
+#' \item{Total.Assays}{???}}
+"biosolids"
+
+#' biosolids_class chemical list from the US EPA CompTox Chemicals Dashboard
+#'
+#' A data set containing a list of 692 chemicals with related information
+#'
+#' \url{https://comptox.epa.gov/dashboard/chemical-lists/BIOSOLIDS}
+#'
+#' @format A csv representing 692 rows and 31 columns
+#' \describe{
+#' \item{INCHIKEY}{A character of the InChIKey identifier}
+#' \item{DTXSID}{A chemical identifier}
+#' \item{PREFERRED.NAME}{A character string of the name}
+#' \item{CASRN}{A character string of the CASRN identifier}
+#' \item{IUPAC.NAME}{A character string of the IUPAC identifier}
+#' \item{SMILES}{A character string of the SMILES identifier}
+#' \item{INCHI.STRING}{A character giving the INCHI string identifier.}
+#' \item{MOLECULAR.FORMULA}{A character string of the molecular formula}
+#' \item{AVERAGE.MASS}{A numeric giving the average atomic mass}
+#' \item{MONOISOTOPIC.MASS}{A numeric giving the monoisotopic mass}
+#' \item{Num.of.SOURCES}{An integer giving the number of sources}
+#' \item{Num.of.PUBMED.ARTICLES}{An integer giving the number of PUBMED
+#' Articles}
+#' \item{PUBCHEM.DATA.SOURCES}{An integer giving the number of PUBCHEM data
+#' sources}
+#' \item{CPDAT.COUNT}{An integer giving the number of occurrences within CPDAT}
+#' \item{QC.Level}{An integer giving the quality control level}
+#' \item{Num.ToxCast.Active}{???}
+#' \item{Pct.ToxCast.Active}{???}
+#' \item{Total.Assays}{???}
+#' \item{kingdom}{A character string giving the ClassyFire kingdom label}
+#' \item{superclass}{A character string giving the ClassyFire superclass label}
+#' \item{class}{A character string giving the ClassyFire class label}
+#' \item{subclass}{A character string giving the ClassyFire subclass label}
+#' \item{level5}{A character string giving the ClassyFire level5 label}
+#' \item{level6}{A character string giving the ClassyFire level6 label}
+#' \item{level7}{A character string giving the ClassyFire level7 label}
+#' \item{level8}{A character string giving the ClassyFire level8 label}
+#' \item{level9}{A character string giving the ClassyFire level9 label}
+#' \item{level10}{A character string giving the ClassyFire level10 label}
+#' \item{level11}{A character string giving the ClassyFire level11 label}
+#' \item{terminal_label}{A character string giving the most specific ClassyFire
+#'  label for classification}
+#' \item{terminal_level}{An integer giving the level of the `terminal_label`}
+#' }
+"biosolids_class"
+
 #' BIOSOLIDS2021 chemical list from the US EPA CompTox Chemicals Dashboard
 #'
 #' A data set containing a list of 726 chemicals with related information
@@ -14,6 +89,84 @@
 #' }
 #' @seealso \code{\link{BIOSOLIDS2021_class}}
 'BIOSOLIDS2021'
+
+#' usgs chemical list from the US EPA CompTox Chemicals Dashboard
+#'
+#' A data set containing a list of 707 chemicals with related information.
+#'
+#' \url{https://comptox.epa.gov/dashboard/chemical-lists/USGSWATER}
+#'
+#' @format A csv representing 707 rows and 18 columns.
+#' \describe{
+#' \item{DTXSID}{A chemical identifier}
+#' \item{PREFERRED.NAME}{A character string of the name}
+#' \item{CASRN}{A character string of the CASRN identifier}
+#' \item{INCHIKEY}{A character of the InChIKey identifier}
+#' \item{IUPAC.NAME}{A character string of the IUPAC identifier}
+#' \item{SMILES}{A character string of the SMILES identifier}
+#' \item{INCHI.STRING}{A character giving the INCHI string identifier.}
+#' \item{MOLECULAR.FORMULA}{A character string of the molecular formula}
+#' \item{AVERAGE.MASS}{A numeric giving the average atomic mass}
+#' \item{MONOISOTOPIC.MASS}{A numeric giving the monoisotopic mass}
+#' \item{Num.of.SOURCES}{An integer giving the number of sources}
+#' \item{Num.of.PUBMED.ARTICLES}{An integer giving the number of PUBMED
+#' Articles}
+#' \item{PUBCHEM.DATA.SOURCES}{An integer giving the number of PUBCHEM data
+#' sources}
+#' \item{CPDAT.COUNT}{An integer giving the number of occurrences within CPDAT}
+#' \item{QC.Level}{An integer giving the quality control level}
+#' \item{Num.ToxCast.Active}{???}
+#' \item{Pct.ToxCast.Active}{???}
+#' \item{Total.Assays}{???}}
+"usgs"
+
+#' usgs chemical list from the US EPA CompTox Chemicals Dashboard
+#'
+#' A data set containing a list of 707 chemicals with related information.
+#'
+#' \url{https://comptox.epa.gov/dashboard/chemical-lists/USGSWATER}
+#'
+#' @format A csv representing 688 rows and 31 columns.
+#' \describe{
+#' \item{INCHIKEY}{A character of the InChIKey identifier}
+#' \item{DTXSID}{A chemical identifier}
+#' \item{PREFERRED.NAME}{A character string of the name}
+#' \item{CASRN}{A character string of the CASRN identifier}
+#' \item{IUPAC.NAME}{A character string of the IUPAC identifier}
+#' \item{SMILES}{A character string of the SMILES identifier}
+#' \item{INCHI.STRING}{A character giving the INCHI string identifier.}
+#' \item{MOLECULAR.FORMULA}{A character string of the molecular formula}
+#' \item{AVERAGE.MASS}{A numeric giving the average atomic mass}
+#' \item{MONOISOTOPIC.MASS}{A numeric giving the monoisotopic mass}
+#' \item{Num.of.SOURCES}{An integer giving the number of sources}
+#' \item{Num.of.PUBMED.ARTICLES}{An integer giving the number of PUBMED
+#' Articles}
+#' \item{PUBCHEM.DATA.SOURCES}{An integer giving the number of PUBCHEM data
+#' sources}
+#' \item{CPDAT.COUNT}{An integer giving the number of occurrences within CPDAT}
+#' \item{QC.Level}{An integer giving the quality control level}
+#' \item{Num.ToxCast.Active}{???}
+#' \item{Pct.ToxCast.Active}{???}
+#' \item{Total.Assays}{???}
+#' \item{kingdom}{A character string giving the ClassyFire kingdom label}
+#' \item{superclass}{A character string giving the ClassyFire superclass label}
+#' \item{class}{A character string giving the ClassyFire class label}
+#' \item{subclass}{A character string giving the ClassyFire subclass label}
+#' \item{level5}{A character string giving the ClassyFire level5 label}
+#' \item{level6}{A character string giving the ClassyFire level6 label}
+#' \item{level7}{A character string giving the ClassyFire level7 label}
+#' \item{level8}{A character string giving the ClassyFire level8 label}
+#' \item{level9}{A character string giving the ClassyFire level9 label}
+#' \item{level10}{A character string giving the ClassyFire level10 label}
+#' \item{level11}{A character string giving the ClassyFire level11 label}
+#' \item{terminal_label}{A character string giving the most specific ClassyFire
+#'  label for classification}
+#' \item{terminal_level}{An integer giving the level of the `terminal_label`}
+#' }
+"usgs_class"
+
+
+
 
 #' USGSWATER chemical list from the US EPA CompTox Chemicals Dashboard
 #'
@@ -138,3 +291,29 @@
 #' \item{colnames}{tip and node labels of chemont tree}
 #' }
 "chemont_jiangconrath_IC_SVH"
+
+#' ChemOnt Taxonomy levels
+#'
+#' A vector containing the eleven taxonomy levels used in the ChemOnt ontology.
+#'
+#' @format A character vector of length eleven.
+"chemont_tax_levels"
+
+#' Chemont tree
+#'
+#' A `phylo` object encoding the ChemOnt ontology in its taxonomy form.
+#'
+#' @format A list of five objects:
+#' \describe{
+#' \item{edge}{A matrix with two columns and row for each edge in the tree.
+#'  The first column is the parent and the second column is the child.}
+#' \item{Nnode}{The number of internal nodes, including the root.}
+#' \item{tip.label}{A vector containing the tip labels corresponding to the
+#'  node numbers.}
+#' \item{edge.length}{A vector containing edge lengths derived in the
+#'  construction of the `phylo` object. These are also useful for creating well
+#'  proportioned tree diagrams.}
+#' \item{node.label}{A vector containing the node labels corresponding to the
+#'   node numbers.}
+#' }
+"chemont_tree"
