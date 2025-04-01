a5f9d92fd7e19453212f5ff5ee7112c8cacf61fd
Ring <Ring.Caroline@epa.gov>
2025-04-01 13:38:35 -0400

    Removed older unused versions of data and updated data documentation

--- R/data.R
+++ R/data.R
@@ -23,9 +23,9 @@
 #' sources}
 #' \item{CPDAT.COUNT}{An integer giving the number of occurrences within CPDAT}
 #' \item{QC.Level}{An integer giving the quality control level}
-#' \item{Num.ToxCast.Active}{???}
-#' \item{Pct.ToxCast.Active}{???}
-#' \item{Total.Assays}{???}}
+#' \item{Num.ToxCast.Active}{An integer: Number of ToxCast assays that showed bioactivity}
+#' \item{Pct.ToxCast.Active}{A numeric: Percent of ToxCast assays that showed bioactivity}
+#' \item{Total.Assays}{An integer: Total number of ToxCast assays in which the chemical was tested}}
 "biosolids"
 
 #' biosolids_class chemical list from the US EPA CompTox Chemicals Dashboard
@@ -53,9 +53,9 @@
 #' sources}
 #' \item{CPDAT.COUNT}{An integer giving the number of occurrences within CPDAT}
 #' \item{QC.Level}{An integer giving the quality control level}
-#' \item{Num.ToxCast.Active}{???}
-#' \item{Pct.ToxCast.Active}{???}
-#' \item{Total.Assays}{???}
+#' \item{Num.ToxCast.Active}{An integer: Number of ToxCast assays that showed bioactivity}
+#' \item{Pct.ToxCast.Active}{A numeric: Percent of ToxCast assays that showed bioactivity}
+#' \item{Total.Assays}{An integer: Total number of ToxCast assays in which the chemical was tested}
 #' \item{kingdom}{A character string giving the ClassyFire kingdom label}
 #' \item{superclass}{A character string giving the ClassyFire superclass label}
 #' \item{class}{A character string giving the ClassyFire class label}
@@ -72,23 +72,6 @@
 #' \item{terminal_level}{An integer giving the level of the `terminal_label`}
 #' }
 "biosolids_class"
-
-#' BIOSOLIDS2021 chemical list from the US EPA CompTox Chemicals Dashboard
-#'
-#' A data set containing a list of 726 chemicals with related information
-#'
-#' \url{https://comptox.epa.gov/dashboard/chemical-lists/BIOSOLIDS2021}
-#'
-#' @format A csv representing 726 rows and 18 columns
-#' \describe{
-#' \item{DTXSID}{A chemical identifier}
-#' \item{PREFERRED_NAME}{A character string of the name}
-#' \item{CASRN}{A character string of the CASRN identifier}
-#' \item{INCHIKEY}{A character of the InChIKey identifier}
-#' \item{SMILES}{A character string of the SMILES identifier}
-#' }
-#' @seealso \code{\link{BIOSOLIDS2021_class}}
-'BIOSOLIDS2021'
 
 #' usgs chemical list from the US EPA CompTox Chemicals Dashboard
 #'
@@ -115,9 +98,9 @@
 #' sources}
 #' \item{CPDAT.COUNT}{An integer giving the number of occurrences within CPDAT}
 #' \item{QC.Level}{An integer giving the quality control level}
-#' \item{Num.ToxCast.Active}{???}
-#' \item{Pct.ToxCast.Active}{???}
-#' \item{Total.Assays}{???}}
+#' \item{Num.ToxCast.Active}{An integer: Number of ToxCast assays that showed bioactivity}
+#' \item{Pct.ToxCast.Active}{A numeric: Percent of ToxCast assays that showed bioactivity}
+#' \item{Total.Assays}{An integer: Total number of ToxCast assays in which the chemical was tested}}
 "usgs"
 
 #' usgs chemical list from the US EPA CompTox Chemicals Dashboard
@@ -145,9 +128,9 @@
 #' sources}
 #' \item{CPDAT.COUNT}{An integer giving the number of occurrences within CPDAT}
 #' \item{QC.Level}{An integer giving the quality control level}
-#' \item{Num.ToxCast.Active}{???}
-#' \item{Pct.ToxCast.Active}{???}
-#' \item{Total.Assays}{???}
+#' \item{Num.ToxCast.Active}{An integer: Number of ToxCast assays that showed bioactivity}
+#' \item{Pct.ToxCast.Active}{A numeric: Percent of ToxCast assays that showed bioactivity}
+#' \item{Total.Assays}{An integer: Total number of ToxCast assays in which the chemical was tested}
 #' \item{kingdom}{A character string giving the ClassyFire kingdom label}
 #' \item{superclass}{A character string giving the ClassyFire superclass label}
 #' \item{class}{A character string giving the ClassyFire class label}
@@ -165,63 +148,6 @@
 #' }
 "usgs_class"
 
-
-
-
-#' USGSWATER chemical list from the US EPA CompTox Chemicals Dashboard
-#'
-#' A data set containing a list of 707 chemicals with related information
-#'
-#' \url{https://comptox.epa.gov/dashboard/chemical-lists/USGSWATER}
-#'
-#' @format A csv representing 707 rows and 18 columns
-#' \describe{
-#' \item{DTXSID}{A chemical identifier}
-#' \item{PREFERRED_NAME}{A character string of the name}
-#' \item{CASRN}{A character string of the CASRN identifier}
-#' \item{INCHIKEY}{A character of the InChIKey identifier}
-#' \item{SMILES}{A character string of the SMILES identifier}
-#' }
-#' @seealso \code{\link{USGSWATER_class}}
-'USGSWATER'
-
-#' BIOSOLIDS2021 chemical list with ClassyFire classifications
-#'
-#' A data set containing a list of 726 chemicals with related information
-#'
-#' Original data source:
-#' \url{https://comptox.epa.gov/dashboard/chemical-lists/BIOSOLIDS2021}.
-#' ClassyFire classifications obtained via ClassyFire API.
-#'
-#' @format A csv representing 726 rows and 18 columns \describe{ \item{DTXSID}{A
-#'   chemical identifier} \item{PREFERRED_NAME}{A character string of the name}
-#'   \item{CASRN}{A character string of the CASRN identifier} \item{INCHIKEY}{A
-#'   character of the InChIKey identifier} \item{SMILES}{A character string of
-#'   the SMILES identifier} \item{kingdom}{ClassyFire kingdom label}
-#'   \item{superclass}{ClassyFire superclass label} \item{class}{ClassyFire
-#'   class label} \item{subclass}{ClassyFire subclass label} \item{level
-#'   5}{ClassyFire level 5 label} \item{level 6}{ClassyFire level 6 label}
-#'   \item{level 7}{ClassyFire level 7 label} \item{level 8}{ClassyFire level 8
-#'   label} \item{level 9}{ClassyFire level 9 label} \item{level 10}{ClassyFire
-#'   level 10 label} \item{level 11}{ClassyFire level 11 label} }
-#' @seealso \code{\link{BIOSOLIDS2021}}
-'BIOSOLIDS2021_class'
-
-#' USGSWATER chemical list with ClassyFire classifications
-#'
-#' A data set containing a list of 707 chemicals with related information
-#'
-#' Original data source:
-#' \url{https://comptox.epa.gov/dashboard/chemical-lists/USGSWATER}. ClassyFire
-#' classifications obtained via ClassyFire API.
-#'
-#' @format A csv representing 707 rows and 18 columns \describe{ \item{DTXSID}{A
-#'   chemical identifier} \item{PREFERRED_NAME}{A character string of the name}
-#'   \item{CASRN}{A character string of the CASRN identifier} \item{INCHIKEY}{A
-#'   character of the InChIKey identifier} \item{SMILES}{A character string of
-#'   the SMILES identifier} }
-#' @seealso \code{\link{USGSWATER}}
-'USGSWATER_class'
 
 #'ChemOnt taxonomy as data.frame
 #'
